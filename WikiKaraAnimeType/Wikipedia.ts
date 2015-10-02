b8a9de13098cdf312192a6ea34780c8b9a2a4ec3
develop <develop@mail.com>
2015-10-02 17:32:26 +0900

    Wikipediaからタイトル情報取得

---
+++ WikiKaraAnimeType/Wikipedia.ts
@@ -0,0 +1,31 @@
+﻿namespace utils.wikipedia {
+    const ENDPOINT = 'https://ja.wikipedia.org/w/api.php?'
+    const INFO_BASE_URL = ENDPOINT + 'action=query&prop=revisions&format=json&rvprop=content&redirects=&titles='
+    const TITLE_BASE_URL = ENDPOINT + 'action=query&list=search&format=json&srsearch='
+
+    export function search(title: string, success: (infos: Array<SearchInfo>) => void, fail?: (jqXhr: JQueryXHR) => void) {
+        var url = TITLE_BASE_URL + encodeURIComponent(title)
+        var xhr = $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'jsonp',
+            jsonpCallback: 'callback'
+        })
+        xhr.then((json: SearchResponseJson) => {
+            success(json.query.search)
+        }, fail)
+    }
+
+    interface SearchResponseJson {
+        query: {
+            search: Array<SearchInfo>
+        }
+    }
+
+    export interface SearchInfo {
+        title: string,
+        snippet: string,
+        wordcount: number,
+        timestamp: string
+    }
+}
---
+++ WikiKaraAnimeType/spec/javascripts/WikipediaSpec.ts
@@ -0,0 +1,26 @@
+﻿namespace WikipediaSpec {
+    describe('Wikipedia', () => {
+        describe('search', () => {
+            var infos: Array<utils.wikipedia.SearchInfo>
+            beforeEach((done) => {
+                utils.wikipedia.search('WORKING', (value) => {
+                    infos = value
+                    done()
+                })
+            })
+            it('Wikipediaからタイトルを検索できる', () => {
+                expect(_.isArray(infos)).toBeTruthy()
+                _.each(infos, (info) => {
+                    expect(_.isEmpty(info)).toBeFalsy()
+                    expect(_.isString(info.title)).toBeTruthy()
+                    expect(_.isString(info.snippet)).toBeTruthy()
+                    expect(_.isString(info.timestamp)).toBeTruthy()
+                    expect(_.isNumber(info.wordcount)).toBeTruthy()
+                    expect(info.wordcount).toBeGreaterThan(0)
+                    var timestamp = new Date(info.timestamp)
+                    expect(_.isDate(timestamp)).toBeTruthy()
+                })
+            })
+        })
+    })
+}
